1e6b747c2034644d79784e16a1dd6b293e5713d0
Горейко Данило Ярославович <I06995@nbu.bank.gov.ua>
2015-11-26 13:58:28 +0200

    Local mashine edit

--- app.js
+++ app.js
@@ -6,15 +6,5 @@ angular.module('weatherApp', [])
     $http.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=df11cc2897e771ab2fb7cbfaf2279ec8').success(function(data){
         $scope.status = data;
     });
-
-//      $http({method: $scope.method, url: $scope.url, responseType: 'json' }).
-        //then(function(response) {
-          //$scope.status = response.status;
-          //$scope.data = response.data;
-        //}, function(response) {
-//          $scope.data = response.data || "Request failed";
-//          $scope.status = response.status;
-//      });
-
     //$scope.status = 'Lviv';
   }]);    
